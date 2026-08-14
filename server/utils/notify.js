cf4ccaff37d3d2c988be0fd11b7a8cb788b8c853
Ehsan <ihsanullahi.rii5@gmail.com>
2026-08-13 20:51:45 -0700

    Fix node_modules and setup backend

--- server/utils/notify.js
+++
@@ -1,9 +0,0 @@
-import Notification from "../models/Notification.js";
-import { emitToUser } from "../socket/index.js";
-
-export const createNotification = async ({ user, sender, type, text, link = "", relatedId = null }) => {
-  const notification = await Notification.create({ user, sender, type, text, link, relatedId });
-  const populated = await notification.populate("sender", "name avatar");
-  emitToUser(user, "notification:new", populated);
-  return populated;
-};
