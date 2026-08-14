11d4af984ee4ab26b538a533f4b1bcc7d4b68075
Ehsan <ihsanullahi.rii5@gmail.com>
2026-08-13 21:22:11 -0700

    first commit

--- server/middleware/admin.js
+++
@@ -1,6 +0,0 @@
-export const adminOnly = (req, res, next) => {
-  if (req.user && req.user.role === "admin") {
-    return next();
-  }
-  return res.status(403).json({ message: "Admin access required" });
-};
